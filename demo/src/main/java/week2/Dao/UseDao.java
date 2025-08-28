b76a1ea62518a697b34d7ca448ea4504dfbe4ee3
24oanh <oanhoanh2004.vn@gmail.com>
2025-08-28 23:02:21 +0700

    Delete demo/src/main/java/week2 directory

--- demo/src/main/java/week2/Dao/UseDao.java
+++
@@ -1,7 +0,0 @@
-package week2.Dao;
-
-import week2.model.User;
-
-public interface UseDao {
-    User get(String username); 
-}
--- demo/src/main/java/week2/services/Useservices.java
+++
@@ -1,8 +0,0 @@
-package week2.services;
-
-import week2.model.User;
-
-public interface Useservices {
-    User login(String username, String password);
-    User get(String username);
-}
